6b4e60de46253c530013167ceec469d2e186cb75
Luckymani <msmanikanta25@gmail.com>
2023-07-08 22:33:50 +0530

    initial setup of the front-end
    3 components added

--- sruthi-botique-frontend/src/App.js
+++ sruthi-botique-frontend/src/App.js
@@ -1,12 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./Components/LoginPage";
+import RegisterPage from "./Components/RegisterPage";
+import Background from "./Components/Background";
 
 function App() {
 	return (
-		<div className="App">
-			<p>Hello React</p>
-
-			<h1>hello this is manikanta</h1>
+		<div className="app">
+			<Background />
+			<Routes>
+				<Route path="/login" element={<LoginPage />}></Route>
+				<Route path="/register" element={<RegisterPage />}></Route>
+			</Routes>
 		</div>
 	);
 }
