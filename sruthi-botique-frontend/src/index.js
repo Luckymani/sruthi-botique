ecae47179d0e4c00e27de486004441b099ddfb86
Luckymani <msmanikanta25@gmail.com>
2023-07-08 22:58:41 +0530

    spelling mistake corrected in index.js

--- sruthi-botique-frontend/src/index.js
+++ sruthi-botique-frontend/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRotuer } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<BrowserRotuer>
+	<BrowserRouter>
 		<App />
-	</BrowserRotuer>
+	</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
